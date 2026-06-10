4318e0c57abb3ad7d872802839af8138cd23ed6a
alijamshidy <kbxxkj@gmail.com>
2026-06-10 19:58:53 +0330

    feat: Add controllers for category, product, seller, card, customer authentication, home, order, and payment functionalities
    - Implemented categoryController for adding and retrieving categories.
    - Created productController for managing products including adding, updating, and retrieving products.
    - Developed sellerController for handling seller requests and status updates.
    - Added cardControllers for managing shopping cart operations and wishlist functionalities.
    - Introduced customerAuthControllers for customer registration, login, and logout processes.
    - Built homeControllers for fetching categories, products, and handling product reviews.
    - Established orderControllers for managing customer and seller orders, including order status updates.
    - Created paymentControllers for integrating Stripe payment processing and account management.

---
+++ controllers/payment/paymentControllers.js
@@ -0,0 +1,51 @@
+const stripeModel = require('../../models/stripeModel')
+const {v4: uuidv4} = require("uuid")
+const { responseReturn } = require("../../utiles/response")
+const stripe  = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+class paymentControllers {
+
+    create_stripe_connect_account = async (req,res) => {
+        const { id } = req
+        const uid = uuidv4()
+
+        try {
+            const stripeInfo = await stripeModel.findOne({ sellerId: id  })
+            if (stripeInfo) {
+            await stripeModel.deleteOne({ sellerId: id })
+            const account = await stripe.accounts.create({ type: 'express' }) 
+            const accountLink = await stripe.accountLinks.create({
+                account: account.id,
+                refresh_url: 'http://localhost:3001/refresh',
+                return_url:  `http://localhost:3001/success?activeCode=${uid}`,
+                type: 'account_onboarding'
+            })
+            await stripeModel.create({
+                sellerId: id,
+                stripeId: account.id,
+                code: uid
+            })
+            responseReturn(res,201,{url:accountLink.url })
+        }else{
+            const account = await stripe.accounts.create({ type: 'express' }) 
+            const accountLink = await stripe.accountLinks.create({
+                account: account.id,
+                refresh_url: 'http://localhost:3001/refresh',
+                return_url:  `http://localhost:3001/success?activeCode=${uid}`,
+                type: 'account_onboarding'
+            })
+ 
+            await stripeModel.create({
+                sellerId: id,
+                stripeId: account.id,
+                code: uid
+            })
+            responseReturn(res,201,{url:accountLink.url })
+        }
+    } catch (error) {
+        console.log('strpe connect account errror' + error.message)
+     }
+    }
+}
+
+module.exports = new paymentControllers()
